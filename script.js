023f6a7a4d244a5152ff453c2075383f44e7d0c8
everjammin <bgallagher@invoca.com>
2023-05-06 16:02:04 -0400

    take two

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log('Hell yea!');
-console.log('Hell no!');
+var msg1 = ['a','b','c','d','e'];
+var msg2 = ['a','b','c','d','e'];
+var msg3 = ['a','b','c','d','e'];
+
+var randomizer = (msg) => {
+    var r = return Math.floor(Math.random() * msg.length);
+    return msg[r];
+}
+
+var finalMsg = randomizer(msg1) + randomizer(msg2) + randomizer(msg3);
+
+console.log(finalMsg);
